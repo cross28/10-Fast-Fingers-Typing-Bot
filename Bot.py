f0717d7e8b9e84777967634ac7d9e305fde17e71
cross28 <isaiahcrosscollege@gmail.com>
2019-01-10 02:30:44 -0600

    added test parses

--- Bot.py
+++ Bot.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen as uReq
 import os
+import json
 import webbrowser
 
 siteurl = 'https://10fastfingers.com/typing-test/english'
@@ -8,8 +9,6 @@ siteurl = 'https://10fastfingers.com/typing-test/english'
 #connecting to page
 site = uReq(siteurl)
 site_html = site.read()
-site.close()
-print(site_html)
+
 soup = BeautifulSoup(site_html, 'html.parser')
-
-
+print(soup.findAll('div',{'id':'ow1'}))
