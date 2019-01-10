1e0e4efabef26bc774894a82073b4c94b355b4b9
cross28 <isaiahcrosscollege@gmail.com>
2019-01-10 03:14:28 -0600

    null

--- Bot.py
+++ Bot.py
@@ -1,5 +1,3 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen as uReq
 import webbrowser
 from selenium import webdriver
 
@@ -11,7 +9,12 @@ driver.get(siteurl)
 driver.implicitly_wait(15)
 pbutton = driver.find_element_by_class_name('highlight')
 print(pbutton.text)
-'''
+
+''' PERSONAL REFERENCE TO BEAUTIFUL SOUP AND REQUESTS
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
+
 #connecting to page and parsing
 site = uReq(siteurl)
 site_html = site.read()
