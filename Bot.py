4caa0742d55d7ab743b5519c723c8b7e1095bcb3
cross28 <isaiahcrosscollege@gmail.com>
2019-01-11 03:31:12 -0600

    Added different timing settings.

--- Bot.py
+++ Bot.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen as uReq
 from selenium import webdriver
 from pynput.keyboard import Key, Controller
+import pynput.mouse as pymouse
 import time
 
 siteurl = 'https://10fastfingers.com/typing-test/english'
 
 keyboard = Controller()
-
+mouse = pymouse.Controller()
 
 #Setting up test browser
 driver = webdriver.Chrome()
@@ -19,10 +20,13 @@ driver.implicitly_wait(15)
 siteReq = uReq(siteurl)
 site = siteReq.read()
 soup = BeautifulSoup(site, 'html.parser')
-inputField = soup.findAll('')
 
+inputField = soup.findAll('span',{'class':'inputfield'})
 word = driver.find_element_by_class_name('highlight')
+
+time.sleep(3)
 while True:
+    soup.
     for a in word.text:
         keyboard.press(a)
         keyboard.release(a)
