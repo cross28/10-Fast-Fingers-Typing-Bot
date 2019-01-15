fe7ad0c0a736c2cf98fb446e2fbbe62c6d96e3ea
cross28 <isaiahcrosscollege@gmail.com>
2019-01-14 23:49:05 -0600

    Got rid of beautifulsoup and request libraries. Modified some of the code that used these libraries.

--- Bot.py
+++ Bot.py
@@ -1,9 +1,6 @@
 import webbrowser
-from bs4 import BeautifulSoup
-from urllib.request import urlopen as uReq
 from selenium import webdriver
 from pynput.keyboard import Key, Controller
-import msvcrt
 import time
 
 siteurl = 'https://10fastfingers.com/typing-test/english'
@@ -15,31 +12,11 @@ driver = webdriver.Chrome()
 driver.get(siteurl)
 driver.implicitly_wait(15)
 
-#Getting html data
-siteReq = uReq(siteurl)
-site = siteReq.read()
-soup = BeautifulSoup(site, 'html.parser')
-
-#word = soup.findAll('div',{'class':'highlight'})
-#inputField = soup.findAll('span', {'class': 'inputfield'})
-
 while True:
     word = driver.find_element_by_class_name('highlight')
+
     for a in word.text:
         keyboard.press(a)
         keyboard.release(a)
     keyboard.press(' ')
-
-
-''' PERSONAL REFERENCE TO BEAUTIFUL SOUP AND REQUESTS
-
-from bs4 import BeautifulSoup
-from urllib.request import urlopen as uReq
-
-#connecting to page and parsing
-site = uReq(siteurl)
-site_html = site.read()
-soup = BeautifulSoup(site_html, 'html.parser')
-
-print(soup.findAll('span',{'class':'highlight'}))
-'''
+    keyboard.release(' ')
