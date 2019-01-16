d3dd86d2f1bf28c5944b71826bf55be0ad26e08d
cross28 <isaiahcrosscollege@gmail.com>
2019-01-15 20:36:57 -0600

    Added adjustable wpm

--- Bot.py
+++ Bot.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from pynput.keyboard import Key, Controller
+import time
 
 siteurl = 'https://10fastfingers.com/typing-test/english'
 
 keyboard = Controller()
+
+choice = input('How many wpm would you like to type (Enter a number or the word max): ')
 
 #Setting up test browser
 driver = webdriver.Chrome()
@@ -12,6 +15,11 @@ driver.implicitly_wait(15)
 
 while True:
     word = driver.find_element_by_class_name('highlight')
+
+    if (choice.upper() == 'MAX'):
+        time.sleep(0)
+    else:
+        time.sleep(60/int(choice))
 
     for a in word.text:
         keyboard.press(a)
